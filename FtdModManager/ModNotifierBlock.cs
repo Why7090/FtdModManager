577dc90094e3d5717edd38188d08039153c90061
Why7090 <whyharry9@gmail.com>
2019-04-04 20:52:55 -0400

    Added Mod Notifier block

---
+++ FtdModManager/ModNotifierBlock.cs
@@ -0,0 +1,31 @@
+﻿using PetaJson;
+using System;
+using System.Collections.Generic;
+
+namespace FtdModManager
+{
+    public class ModNotifierBlock : BlockWithText
+    {
+        // Mod Name, modmanifest.json URI, Description
+        public List<Tuple<string, Uri, string>> mods;
+
+        public override void BlockStart()
+        {
+            base.BlockStart();
+            if (mods == null)
+                mods = new List<Tuple<string, Uri, string>>();
+        }
+
+        public override bool StillSavingStringsLikeThis => false;
+
+        public override string GetText()
+        {
+            return Json.Format(mods, JsonOptions.DontWriteWhitespace);
+        }
+        public override string SetText(string str)
+        {
+            mods = Json.Parse<List<Tuple<string, Uri, string>>>(str);
+            return "";
+        }
+    }
+}
